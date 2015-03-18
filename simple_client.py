597d75c8da835829b06322caaa683cf7a083c571
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-03-18 13:37:09 -0700

    improve server basics

--- simple_client.py
+++ simple_client.py
@@ -3,8 +3,28 @@ from json import dumps
 
 session = requests.Session()
 server_endpoint = 'http://localhost:8080'
+
+# On launch the client:
+    # 1. Gets user data from the ._user file
+    # 2. Contacts Star Conflict server and gets game data
+    # 3. Allows user to enter a game or start a new one
+    # 4. Allows users to leave the app.
+
+# In a game users can:
+    # 0. See the current game state
+    # 1. Leave the game
+    # 2. If it is their turn:
+        # a. Take an action
+        # b. End turn
+
+
+
+
+
+
 response = session.get(
         server_endpoint,
         data=dumps(1)
     )
+
 print response.content
--- simple_server.py
+++ simple_server.py
@@ -1,10 +1,26 @@
 from gevent import pywsgi
+import redis
+import routes
+
+def handler(environ, start_response):
+#    g = {
+#        'redis': redis.StrictRedis(host=kwargs['redis'])
+#         }
+    #status, output, headers = dispatch(environ, g, routes.__dict__)
+    status = '200 OK'
+    headers = [('Content-Type', 'text/html')]
+    start_response(status, headers)
+#output = 'Play Star Conflict!'
+    return '<b>Play Star Conflict!</b>\n'
+    
+#   return output
 
 def hello_world(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+    print environ
     return '<b>Play Star Conflict!</b>\n'
 
 server = pywsgi.WSGIServer(
-    ('', 8080), hello_world)
+    ('', 8080), handler).serve_forever()
 
 server.serve_forever()
