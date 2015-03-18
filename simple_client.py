0f6a093fd2ea1afd4f35ec34f11a66413849f13c
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-03-18 13:47:18 -0700

    sketch client code

--- simple_client.py
+++ simple_client.py
@@ -5,17 +5,18 @@ session = requests.Session()
 server_endpoint = 'http://localhost:8080'
 
 # On launch the client:
-    # 1. Gets user data from the ._user file
-    # 2. Contacts Star Conflict server and gets game data
-    # 3. Allows user to enter a game or start a new one
-    # 4. Allows users to leave the app.
+    # 1. Gets user data from the ._user file (local operation)
+        # a. If no user account is present, make a user account (POST)
+    # 2. Contacts Star Conflict server and gets game data (GET)
+    # 3. Allows user to enter a game or start a new one (local operation/POST)
+    # 4. Allows users to leave the app (local operation).
 
 # In a game users can:
-    # 0. See the current game state
-    # 1. Leave the game
+    # 0. See the current game state (GET)
+    # 1. Leave the game (local operation)
     # 2. If it is their turn:
-        # a. Take an action
-        # b. End turn
+        # a. Take an action (POST)
+        # b. End turn (POST)
 
 response = session.get(
         server_endpoint,
